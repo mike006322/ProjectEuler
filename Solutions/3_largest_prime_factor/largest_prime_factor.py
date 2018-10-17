e9b61797c6a32f4d263b4eea49a87f923645487c
Michael Angel <mike006322@gmail.com>
2018-10-16 23:35:57 -0700

    added tests to largest_prime_factor

--- Solutions/3_largest_prime_factor/largest_prime_factor.py
+++ Solutions/3_largest_prime_factor/largest_prime_factor.py
@@ -8,9 +8,9 @@ import sys
 # 1.) First is that if you increment from the smallest prime dividing the primes away from n then the next smallest divisor you find of n will be prime.
 # 2.) Second is that if there is a single prime factor greater than sqrt(n) then after the loop n will equal it
 
-def biggest_prime_factor(n):
+def largest_prime_factor(n):
     max = n
-    """test numbers up to square root of n. Divide n by factors as you go to skip over composite numbers"""
+    # test numbers up to square root of n. Divide n by factors as you go to skip over composite numbers
     if n % 2 == 0:
         while n % 2 == 0:
             n /= 2
@@ -31,7 +31,9 @@ def biggest_prime_factor(n):
     else:
         return int(res)
 
-t = int(input().strip())
-for a0 in range(t):
-    n = int(input().strip())
-    print(biggest_prime_factor(n))
+if __name__ == '__main__':
+    print(largest_prime_factor(600851475143))
+    # t = int(input().strip())
+    # for a0 in range(t):
+    #     n = int(input().strip())
+    #     print(largest_prime_factor(n))
