9e284e1b72c8be5bde59bfee0f3c4c6ee6b9f674
Michael Angel <mike006322@gmail.com>
2018-10-16 23:26:31 -0700

    update to even_fibonacci_numbers. added tests

--- Solutions/2_even_fibonacci_numbers/even_fibonacci_numbers.py
+++ Solutions/2_even_fibonacci_numbers/even_fibonacci_numbers.py
@@ -2,21 +2,24 @@
 
 # #https://www.hackerrank.com/contests/projecteuler/challenges/euler002
 
-import sys
 from functools import lru_cache
 
 @lru_cache(maxsize=None)
-def fib(num):
-    if num == 1:
+def fib(n):
+    """
+    returns the nth fibonacci number
+    """
+    if n == 1:
         return 1
-    if num == 2:
+    if n == 2:
         return 2
-    if num > 2:
-        return fib(num - 1) + fib(num - 2)
+    if n > 2:
+        return fib(n - 1) + fib(n - 2)
 
-t = int(input().strip())
-for a0 in range(t):
-    n = int(input().strip())
+def even_fibonacci_numbers(n):
+    """
+    returns the sum of the even valued fibonacci numbers less than or equal to n
+    """
     even_fib_lessthan_n = []
     i = 1
     fib_i = 1
@@ -25,7 +28,20 @@ for a0 in range(t):
             even_fib_lessthan_n.append(fib_i)
         i += 1
         fib_i = fib(i)
-    print(sum(even_fib_lessthan_n))
+    return sum(even_fib_lessthan_n)
 
-#for i in range(6):
-    #print(fib(i))
+
+if __name__ == '__main__':
+    print(even_fibonacci_numbers(4000000))
+    # t = int(input().strip())
+    # for a0 in range(t):
+    #     n = int(input().strip())
+    #     even_fib_lessthan_n = []
+    #     i = 1
+    #     fib_i = 1
+    #     while fib_i < n:
+    #         if fib_i % 2 == 0:
+    #             even_fib_lessthan_n.append(fib_i)
+    #         i += 1
+    #         fib_i = fib(i)
+    #     print(sum(even_fib_lessthan_n))
