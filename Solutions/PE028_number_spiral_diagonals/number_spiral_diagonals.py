7bbb75b5167e609b1c8fca5ee423e967c62c0691
Michael Angel <mike006322@gmail.com>
2018-11-01 17:16:57 -0700

    comments updated

--- Solutions/PE028_number_spiral_diagonals/number_spiral_diagonals.py
+++ Solutions/PE028_number_spiral_diagonals/number_spiral_diagonals.py
@@ -11,7 +11,7 @@ Equations for diagonals:
 4*n**2 + 1
 
 sum_1_to_n(4*n**2 - 2*n + 1) = 4*sum_1_to_n(n**2) - 2*sum_1_to_n(n) + sum_1_to_n(1)
-formula for sum of squares: http://mathforum.org/library/drmath/view/56920.html
+formula for sum of squares: https://brilliant.org/wiki/sum-of-n-n2-or-n3/
 sum_1_to_n(i**2): (n*(n + 1)*(2n + 1))//6
 sum_1_to_n(i): n*(n+1)//2
 
@@ -21,7 +21,6 @@ The sum is all of the diagonals plus the middle 1
 sum_of_all_diagonals = sum_1_to_N//2(4*i**2 - 2*i + 1 + 4*i**2 + 4*i + 1 + 4*i**2 + 2*i + 1 + 4*i**2 + 1) + 1
 = sum_1_to_N//2(16*i**2 + 4*i + 4) + 1
 = 16*sum_1_to_N//2(i**2) + 4*sum_1_to_N//2(i) + 4*sum_1_to_N//2(1) + 1
-** here is the disconnect
 = 16*(N-1)*(N+1)*N/24 + 4*(N-1)(N+1)/8 + 4*(N-1)/2 + 1
 = 2*(N-1)*(N+1)*N/3 + (N-1)(N+1)/2 + 2*(N-1) + 1
 = 2*(N-1)*(N+1)*N/3 + (N-1)(N+1)/2 + 2*N - 1
