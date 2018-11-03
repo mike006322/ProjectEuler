77f573ea86b363c182b4488484e8fa26341fb533
Michael Angel <mike006322@gmail.com>
2018-11-02 21:27:19 -0700

    readability, PEP8

--- Solutions/PE002_even_fibonacci_numbers/test_even_fibonacci_numbers.py
+++ Solutions/PE002_even_fibonacci_numbers/test_even_fibonacci_numbers.py
@@ -1,19 +1,21 @@
 import unittest
-import even_fibonacci_numbers
+from even_fibonacci_numbers import *
+
 
 class Test(unittest.TestCase):
 
     def test_fib(self):
         # 1, 2, 3, 5, 8, 13, 21, 34
-        self.assertEqual(even_fibonacci_numbers.fib(1), 1)
-        self.assertEqual(even_fibonacci_numbers.fib(2), 2)
-        self.assertEqual(even_fibonacci_numbers.fib(3), 3)
-        self.assertEqual(even_fibonacci_numbers.fib(4), 5)
-        self.assertEqual(even_fibonacci_numbers.fib(5), 8)
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(2), 2)
+        self.assertEqual(fib(3), 3)
+        self.assertEqual(fib(4), 5)
+        self.assertEqual(fib(5), 8)
 
     def test_even_fibonacci_numbers(self):
-        self.assertEqual(even_fibonacci_numbers.even_fibonacci_numbers(10), 10)
-        self.assertEqual(even_fibonacci_numbers.even_fibonacci_numbers(8), 10)
+        self.assertEqual(even_fibonacci_numbers(10), 10)
+        self.assertEqual(even_fibonacci_numbers(8), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
