b95dba040579b6b71e6dd2ecba4a42a699d88a05
Michael Angel <mike006322@gmail.com>
2018-11-24 09:26:40 -0800

    refactored

--- Solutions/PE033_digit_canceling_fractions/digit_canceling_fractions.py
+++ Solutions/PE033_digit_canceling_fractions/digit_canceling_fractions.py
@@ -2,49 +2,23 @@
 
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler033
 
-from itertools import combinations_with_replacement as combs
 from itertools import combinations
-from itertools import permutations
-from itertools import product
 from math import gcd
 
-
-def have_i_digits_in_common(n, m, i):
-    """
-    input two integers and i
-    return boolean whether they share i digits in common excluding 0's
-    """
-    n = list(n)
-    m = list(m)
-    num_common = 0
-    for digit in n:
-        if digit in m and digit != '0' and digit != 0:
-            m.remove(digit)
-            num_common += 1
-            if num_common >= i:
-                return True
-    return False
-
-
-def find_common_digits(n, m):
-    """
-    input two integers and i
-    returns list common digits as str's excluding 0's
-    """
-    n = list(n)
-    m = list(m)
-    res = []
-    for digit in n:
-        if digit in m:
-            m.remove(digit)
-            if digit != 0:
-                res.append(digit)
-    return res
+# Explanation: Consider that we want solutions "n/d = n2/d2".
+# Iterate through possible values of n which has N digits.
+# Then iterate over N-K possible digits from the digits of n (N choose N-K) to make n2.
+# Now, consider n/n2. We must have n/n2 = d/d2 and d must have the digit/s of n that n2 doesn't have.
+# The clever step is to iterate through fractions that are equal to n/n2;
+# this means divide both n and n2 by g = gcd(n, n2)
+# and then multiply both n/g and n2 /g by integer i, which you will iterate.
+# If we write n/n2 as an ordered pair (n, n2) then the iteration looks like ((n/g)*i, (n2/g)*i), i += 1,
+# and for each iteration of i you check if (n/g)*i has the digits of n that n2 doesn't have.
 
 
 def make_number(digits):
     """
-    input tuple of digits
+    input tuple of digits (base 10)
     return int of those digits
     """
     res = 0
@@ -55,16 +29,24 @@ def make_number(digits):
     return res
 
 
-def find_indicies(number, digit):
+def make_digits(n):
     """
-    input number as a tuple of strings representing digits and one str digit
-    returns all the indicies of the occurances of digit in number
+    input integer
+    output tuple of digits of integer (base 10)
     """
-    indicies = []
+    return tuple(int(x) for x in str(n))
+
+
+def find_indices(number, digit):
+    """
+    input number as an iterable of digits
+    returns all the indices of the occurrences of digit in number
+    """
+    indices = []
     for i, n in enumerate(number):
         if n == digit:
-            indicies.append(i)
-    return indicies
+            indices.append(i)
+    return indices
 
 
 def remove_digits(n, digits):
@@ -79,8 +61,8 @@ def remove_digits(n, digits):
         d = common_digits.pop()
         new_res = set()
         for number in res:
-            occurances = find_indicies(number, d)
-            for i in occurances:
+            occurrences = find_indices(number, d)
+            for i in occurrences:
                 if i == 0:
                     new_res.add(number[1:])
                 elif i == len(number)-1:
@@ -95,192 +77,9 @@ def remove_digits(n, digits):
     return n_res
 
 
-def merge_lists(list1, list2, comb):
-    """
-    input three lists. Third list represents the spots to put elements of the second list
-    merges list2 into list1 based on comb
-    """
-    comb = list(comb)
-    res = []
-    list2 = list(list2)
-    for i in range(len(list1)):
-        while i in comb:
-            res.append(list2.pop(0))
-            comb.remove(i)
-        res.append(list1[i])
-    while list2:
-        res.append(list2.pop(0))
-    return tuple(res)
-
-
-def permutations_with_order(digits, i):
-    """
-    input the digits to be included in result and the number of positions to add digits to
-    returns digits with i more spots filled with digits 1 to 9 and the extra digits used
-    """
-    # first find which spots will have digits added to them
-    # len(digits) + 1 many places to put i digits
-    # comb in combinations(range(len(digits)), i)
-    # above will tell you the spots to put extra digits
-    # eg, (0,1) means add digits to the front and after the first item
-    for comb in combs(range(len(digits) + 1), i):
-        for extra_digits in combs((1, 2, 3, 4, 5, 6, 7, 8, 9), i):
-            for p_extra_digits in permutations(extra_digits, len(extra_digits)):
-                yield merge_lists(digits, p_extra_digits, comb), p_extra_digits
-
-
-def permutations_with_order_given_extras(digits, extras):
-    """
-    input the digits to be included in result and the number of positions to add digits to
-    returns digits with i more spots filled with digits 1 to 9
-    """
-    # first find which spots will have digits added to them
-    # len(digits) + 1 many places to put i digits
-    # comb in combinations(range(len(digits)), i)
-    # above will tell you the spots to put extra digits
-    # eg, (0,1) means add digits to the front and after the first item
-    for comb in combinations(range(len(digits) + 1), len(extras)):
-        for extra_digits in combs(extras, len(extras)):
-            yield merge_lists(digits, extra_digits, comb)
-
-
-def digit_canceling_fractions2(N, K):
-    """
-    passes test cases 0, 1, 2. Times out for 3, 4, 5
-    """
-    res = []
-    fractions = set()
-    test_res = []
-    tested = set()
-    for n in range(10**(N-K-1), 10**(N-K)):
-        for m in range(n + 1, 10**(N-K)):
-            fractions.add((n, m))
-    for fraction in fractions:
-        for numerator_digits, extras in permutations_with_order([int(x) for x in str(fraction[0])], K):
-            numerator = make_number(numerator_digits)
-            for denominator_digits in find_denominator_digits(extras, fraction[1]):
-                denominator = make_number(denominator_digits)
-                if (numerator, denominator, fraction[0], fraction[1]) in tested:
-                    continue
-                tested.add((numerator, denominator, fraction[0], fraction[1]))
-                if denominator > numerator:
-                    if numerator/denominator == fraction[0]/fraction[1]:
-                        test_res.append((numerator, denominator, fraction[0], fraction[1]))
-                        res.append((numerator, denominator))
-    return test_res, sum(x[0] for x in res), sum(x[1] for x in res)
-
-
-def make_fractions(n):
-    """
-    returns list of fractions in form [a,b] where b has at most n digits
-    """
-    res = []
-    for b in range(10**(n-1), 10**n):
-        for a in range(10**(n-1), b):
-            res.append([a, b])
-    return res
-
-
-def digit_canceling_fractions4(N, K):
-    """
-    works for test cases 0, 1, 2, times out for 3, 4, 5
-    """
-    res = []
-    numerator_sum = 0
-    denominator_sum = 0
-    all_digits_in_top = list(product(range(10), repeat=N))
-    for digits_in_bottom in product(range(10), repeat=N):
-        if digits_in_bottom[0] == 0:
-            continue
-        for digits_in_top in all_digits_in_top:
-            if digits_in_top[0] == 0:
-                continue
-            a = make_number(digits_in_top)
-            b = make_number(digits_in_bottom)
-            if a >= b:
-                break
-            shared_digits = find_common_digits(digits_in_top, digits_in_bottom)
-            if len(shared_digits) >= K:
-                for digits_to_remove in set(combinations(shared_digits, K)):
-                    numerators = remove_digits(digits_in_top, digits_to_remove)
-                    denominators = remove_digits(digits_in_bottom, digits_to_remove)
-                    for numerator in numerators:
-                        for denominator in denominators:
-                            if a*denominator == numerator*b:
-                                numerator_sum += a
-                                denominator_sum += b
-                                res.append((a, b, numerator, denominator))
-    return sum(x[0] for x in res), sum(x[1] for x in res), res
-    # return numerator_sum, denominator_sum
-
-
-def find_digits_to_cancel(numerator_tuple, specific_eq_numerator_tuple):
-    res = list(numerator_tuple)
-    for x in specific_eq_numerator_tuple:
-        res.remove(x)
-    return res
-
-
-def find_denominator_digits(digits_to_cancel, eq_denominator):
-    """
-    put the digits to cancel among the eq_denominoator digits which must maintain order
-    """
-    res = set()
-    for p_digits_to_cancel in permutations(digits_to_cancel, len(digits_to_cancel)):
-        for comb in combs(range(len(p_digits_to_cancel)+1), len(str(eq_denominator))):
-            res.add(merge_lists(p_digits_to_cancel, [x for x in str(eq_denominator)], comb))
-    return res
-
-
-def digit_canceling_fractions5(N, K):
-    """
-    passes test cases 0, 1, and 2. Times out for 3, 4, 5
-    """
-    res = []
-    numerators_seen = set()
-    for numerator in range(10**(N-1), 10**N):
-        numerator_tuple = tuple(x for x in str(numerator))
-        for eq_numerator_tuple in combinations(numerator_tuple, N-K):
-            if eq_numerator_tuple == ('0',):
-                continue
-            # for specific_eq_numerator_tuple in permutations(eq_numerator_tuple, N-K):
-            digits_to_cancel = find_digits_to_cancel(numerator_tuple, eq_numerator_tuple)
-            if '0' in digits_to_cancel:
-                continue
-            eq_numerator = make_number(eq_numerator_tuple)
-            if (numerator, eq_numerator) in numerators_seen:
-                continue
-            numerators_seen.add((numerator, eq_numerator))
-            for eq_denominator in range(eq_numerator, 10**(N-K)):
-                if eq_denominator <= eq_numerator:
-                    continue
-                # determine denominator digits
-                # for each permutation of the denominator
-                denominator_tuples = find_denominator_digits(digits_to_cancel, eq_denominator)
-                for specific_denominator_tuple in denominator_tuples:
-                    denominator = make_number(specific_denominator_tuple)
-                    if denominator > numerator:
-                        if numerator*eq_denominator == denominator*eq_numerator:
-                            res.append((numerator, denominator, eq_numerator, eq_denominator))
-    return sum(x[0] for x in res), sum(x[1] for x in res), res
-
-
-# below are the most time-efficient solutions:
-
-# Explanation: Consider that we want solutions "n/d = n2/d2".
-# Iterate through possible values of n which has N digits.
-# Then iterate over N-K possible digits from the digits of n (N choose N-K) to make n2.
-# Now, consider n/n2. We must have n/n2 = d/d2 and d must have the digit/s of n that n2 doesn't have.
-# The clever step is to iterate through fractions that are equal to n/n2;
-# this means divide both n and n2 by g = gcd(n, n2)
-# and then multiply both n/g and n2 /g by integer i, which you will iterate.
-# If we write n/n2 as an ordered pair (n, n2) then the iteration looks like ((n/g)*i, (n2/g)*i), i += 1,
-# and for each iteration of i you check if (n/g)*i has the digits of n that n2 doesn't have.
-
-
 def has_digits(number, digits):
     """
-    return boolean whether number has digits in the order that digits is in
+    return boolean whether number has digits
     """
     digits = list(digits)
     num = [int(x) for x in str(number)]
@@ -293,113 +92,52 @@ def has_digits(number, digits):
     return True
 
 
-def digit_canceling_fractions_4_3():
-    res = set()
-    for numerator in range(10**3, 10**4):
-        numerator_tuple = tuple(int(x) for x in str(numerator))
-        if 0 in numerator_tuple:
-            continue
-        for digit in set(numerator_tuple):
-            if digit == 0:
-                continue
-            g = gcd(numerator, digit)
-            reduced = (numerator // g, digit // g)
-            z = 2
-            check_digits = list(numerator_tuple)
-            check_digits.remove(digit)
-            while len(str(reduced[0]*z)) <= 4 and len(str(reduced[1]*z)) <= 1:
-                if z == g:
-                    z += 1
-                    continue
-                if len(str(reduced[0]*z)) == 4:
-                    if has_digits(reduced[0]*z, check_digits):
-                        e = [int(x) for x in str(reduced[0]*z)]
-                        for x in check_digits:
-                            e.remove(x)
-                        e = e.pop()
-                        if e in [int(x) for x in str(reduced[1]*z)] and e > digit:
-                            res.add((numerator, reduced[0]*z, digit, reduced[1]*z))
-                # else:
-                #     print(reduced[0]*z)
-                z += 1
-    return sum(x[0] for x in res), sum(x[1] for x in res), res
+def find_shared_digits(numerator_digits, numerator2_digits):
+    """
+    returns numerator digits without numerator2 digits
+    """
+    shared_digits = list(numerator_digits)
+    for digit in numerator2_digits:
+                shared_digits.remove(digit)
+    return shared_digits
 
 
-def digit_canceling_fractions_4_1():
+def digit_canceling_fractions(N, K):
+    """
+    Finds numbers such that numerator/denominator = numerator2/denominator2
+    numerator and denominator have N digits
+    numerator and denominator share K digits
+    numerator2 and denominator2 have N-K digits
+    returns the sum of all the numerators, sum of all the denominators
+    """
     res = set()
-    for numerator in range(10**3, 10**4):
-        numerator_tuple = tuple(int(x) for x in str(numerator))
-        for eq_numerator_tuple in set(combinations(numerator_tuple, 3)):
-            eq_numerator = make_number(eq_numerator_tuple)
-            if eq_numerator == 0:
+    for numerator in range(10**(N-1), 10**N):
+        numerator_digits = make_digits(numerator)
+        for numerator2_digits in set(combinations(numerator_digits, N-K)):
+            numerator2 = make_number(numerator2_digits)
+            if numerator2 == 0:
                 continue
-            g = gcd(numerator, eq_numerator)
-            reduced = (numerator // g, eq_numerator // g)
+            g = gcd(numerator, numerator2)
+            reduced = (numerator // g, numerator2 // g)
+            shared_digits = find_shared_digits(numerator_digits, numerator2_digits)
+            if 0 in shared_digits:
+                continue
             z = 1
-            check_digits = list(numerator_tuple)
-            for digit in eq_numerator_tuple:
-                check_digits.remove(digit)
-            if check_digits == [0]:
-                continue
-            while len(str(reduced[0]*z)) <= 4 and len(str(reduced[1]*z)) <= 3:
+            while len(str(reduced[0]*z)) <= N and len(str(reduced[1]*z)) <= N-K:
                 if z == g:
                     z += 1
                     continue
-                reduced_0_z = reduced[0]*z
-                reduced_1_z = reduced[1]*z
-                if check_digits[0] in [int(x) for x in str(reduced_0_z)]:
-                    eq_denominator_tuple = [int(x) for x in str(reduced_0_z)]
-                    for eq_d in remove_digits(tuple(eq_denominator_tuple), check_digits):
-                        # eq_denominator_tuple.remove(check_digits[0])
-                        # eq_denominator = make_number(eq_d)
-                        if reduced_1_z == eq_d and eq_d > eq_numerator:
-                            # res.add((numerator, reduced_0_z, eq_numerator, reduced_1_z))
-                            res.add((numerator, reduced_0_z))
+                reduced0_z = reduced[0]*z
+                reduced1_z = reduced[1]*z
+                if has_digits(reduced0_z, shared_digits):
+                    reduced0_z_digits = make_digits(reduced0_z)
+                    for denominator2 in remove_digits(reduced0_z_digits, shared_digits):
+                        if reduced1_z == denominator2 and denominator2 > numerator2:
+                            res.add((numerator, reduced0_z))
                 z += 1
-    return sum(x[0] for x in res), sum(x[1] for x in res), res
-
-
-def digit_canceling_fractions_4_2():
-    res = set()
-    for numerator in range(10**3, 10**4):
-        numerator_tuple = tuple(int(x) for x in str(numerator))
-        for eq_numerator_tuple in set(combinations(numerator_tuple, 2)):
-            eq_numerator = make_number(eq_numerator_tuple)
-            if eq_numerator == 0:
-                continue
-            g = gcd(numerator, eq_numerator)
-            reduced = (numerator // g, eq_numerator // g)
-            z = 1
-            check_digits = list(numerator_tuple)
-            for digit in eq_numerator_tuple:
-                check_digits.remove(digit)
-            if 0 in check_digits:
-                continue
-            while len(str(reduced[0]*z)) <= 4 and len(str(reduced[1]*z)) <= 2:
-                if z == g:
-                    z += 1
-                    continue
-                reduced_0_z = reduced[0]*z
-                reduced_1_z = reduced[1]*z
-                if has_digits(reduced_0_z, check_digits):
-                    eq_denominator_tuple = [int(x) for x in str(reduced_0_z)]
-                    for eq_d in remove_digits(tuple(eq_denominator_tuple), check_digits):
-                        # eq_denominator_tuple.remove(check_digits[0])
-                        # eq_denominator = make_number(eq_d)
-                        if reduced_1_z == eq_d and eq_d > eq_numerator:
-                            # res.add((numerator, reduced_0_z, eq_numerator, reduced_1_z))
-                            res.add((numerator, reduced_0_z))
-                z += 1
-    return sum(x[0] for x in res), sum(x[1] for x in res), res
+    return sum(x[0] for x in res), sum(x[1] for x in res)
 
 
 if __name__ == '__main__':
     N, K = map(int, input().split())
-    if N <= 3:
-        print(*digit_canceling_fractions5(N, K)[:2])
-    elif K == 3:
-        print(*digit_canceling_fractions_4_3()[:2])
-    elif K == 1:
-        print(*digit_canceling_fractions_4_1()[:2])
-    elif K == 2:
-        print(*digit_canceling_fractions_4_2()[:2])
+    print(*digit_canceling_fractions(N, K))
--- Solutions/PE033_digit_canceling_fractions/test_digit_canceling_fractions.py
+++ Solutions/PE033_digit_canceling_fractions/test_digit_canceling_fractions.py
@@ -1,117 +1,33 @@
 import unittest
 from digit_canceling_fractions import *
-import time
 
 
 class Test(unittest.TestCase):
 
-    # def test_have_i_digits_in_common(self):
-    #     self.assertEqual(have_i_digits_in_common(2, 2, 1), True)
-    #     self.assertEqual(have_i_digits_in_common(22, 22, 2), True)
-    #     self.assertEqual(have_i_digits_in_common(22, 23, 2), False)
-    #     self.assertEqual(have_i_digits_in_common(23, 23, 2), True)
-    #     self.assertEqual(have_i_digits_in_common(220, 220, 3), False)
-    #     self.assertEqual(have_i_digits_in_common(20, 12, 1), True)
-    #     self.assertEqual(have_i_digits_in_common(203, 102, 2), False)
+    def test_remove_digits(self):
+        self.assertEqual(remove_digits((2, 2), [2]), [2])
+        self.assertEqual(remove_digits((1, 2), [1]), [2])
+        # self.assertEqual(remove_digits(202, ['2']), [20, 2] or [20, 2])
+        self.assertEqual(remove_digits((1, 8, 7), [1, 8]), [7])
+        self.assertEqual(remove_digits((3, 3, 2), [3]), [32])
+        self.assertEqual(remove_digits((6, 6, 4), [6]), [64])
 
-    # def test_find_common_digits(self):
-    #     self.assertEqual(find_common_digits((2, ), (2, )), [2])
-    #     self.assertEqual(find_common_digits((2, 0, 1), (2, 1, 0)), [2, 1])
-    #     self.assertEqual(find_common_digits((2, 0), (2, 0)), [2])
-    #
-    # def test_remove_digits(self):
-    #     self.assertEqual(remove_digits((2, 2), [2]), [2])
-    #     self.assertEqual(remove_digits((1, 2), [1]), [2])
-    #     # self.assertEqual(remove_digits(202, ['2']), [20, 2] or [20, 2])
-    #     self.assertEqual(remove_digits((1, 8, 7), [1, 8]), [7])
-    #     print(remove_digits((3, 3, 2), [3]))
-    #     print(remove_digits((6, 6, 4), [6]))
-    #
-    # def test_find_indicies(self):
-    #     self.assertEqual(find_indicies(('2', '2'), '2'), [0, 1])
-    #
-    # def test_digit_canceling_fractions(self):
-    #     # self.assertEqual(digit_canceling_fractions(2, 1), (110, 322))
-    #     print(digit_canceling_fractions(2, 1))
-    #     print(digit_canceling_fractions(3, 1))
+    def test_find_indicies(self):
+        self.assertEqual(find_indices(('2', '2'), '2'), [0, 1])
 
-    # def test_merge_lists(self):
-    #     self.assertEqual(merge_lists([1, 2, 3], ['a', 'b'], [0, 3]), ('a', 1, 2, 3, 'b'))
-    #
-    # def test_find_denominator_digits(self):
-    #     self.assertEqual(find_denominator_digits(['3', '6'], 2), [('2', '3', '6'), ('3', '2', '6'), ('3', '6', '2'), ('2', '6', '3'), ('6', '2', '3'), ('6', '3', '2')])
-    #
-    # def test_digit_canceling_fractions2(self):
-    #     # print(digit_canceling_fractions2(3, 2))
-    #     # print(digit_canceling_fractions5(3, 2))
-    #     test_res, n_sum, d_sum = digit_canceling_fractions2(3, 2)
-    #     # print(n_sum, d_sum)
-    #     # # n_sum, d_sum, test_res = digit_canceling_fractions4(3, 2)
-    #     tr = set(test_res)
-    #     # # print(digit_canceling_fractions4(3, 1))
-    #     # # print(digit_canceling_fractions5(3, 1))
-    #     #
-    #     print(set(digit_canceling_fractions5(3, 2)[2]).difference(tr))
-    #     print(tr.difference(set(digit_canceling_fractions5(3, 2)[2])))
-    #
-        # for i in test_res:
-        #     print(i)
+    def test_has_digits(self):
+        self.assertTrue(has_digits(132, (1, 3)))
+        self.assertTrue(has_digits(7231, (1, 3, 2)))
+        self.assertTrue(has_digits(6937, (3, 9, 6)))
+        self.assertTrue(has_digits(693, (3, 9, 6)))
+        self.assertTrue(has_digits(8693, (3, 9, 6)))
 
-    # def test_permutations_with_order(self):
-    #     self.assertEqual(list(permutations_with_order((1,), 1)), [((1, 1), (1,)), ((2, 1), (2,)), ((3, 1), (3,)), ((4, 1), (4,)), ((5, 1), (5,)), ((6, 1), (6,)), ((7, 1), (7,)), ((8, 1), (8,)), ((9, 1), (9,)), ((1, 1), (1,)), ((1, 2), (2,)), ((1, 3), (3,)), ((1, 4), (4,)), ((1, 5), (5,)), ((1, 6), (6,)), ((1, 7), (7,)), ((1, 8), (8,)), ((1, 9), (9,))])
-
-    # def test_make_fractions(self):
-    #     self.assertEqual(len(make_fractions(2)), 4005)
-    #     self.assertEqual(type(make_fractions(2)[0]), list)
-    #     print(make_fractions(1))
-    #
-
-    # def test_digit_canceling_fractions4(self):
-    #     print(digit_canceling_fractions4(2, 1))
-    #     # start = time.time()
-    #     # print(digit_canceling_fractions4(3, 1))
-    #     # end = time.time()
-    #     # elapsed = end - start
-    #     # print('time elapsed: ', elapsed)
-
-    # def test_digit_canceling_fractions5(self):
-    #     # print(digit_canceling_fractions5(2, 1))
-    #     start = time.time()
-    #     print(digit_canceling_fractions5(4, 3))
-    #     end = time.time()
-    #     elapsed = end - start
-    #     print('time elapsed: ', elapsed)
-
-    # def test_has_digits(self):
-    #     self.assertTrue(has_digits(132, (1, 3)))
-    #     self.assertTrue(has_digits(7231, (1, 3, 2)))
-    #     self.assertTrue(has_digits(6937, (3, 9, 6)))
-    #     self.assertTrue(has_digits(693, (3, 9, 6)))
-    #     self.assertTrue(has_digits(8693, (3, 9, 6)))
-
-    # def test_digit_canceling_fractions_4_3(self):
-    #     # tr = set(digit_canceling_fractions5(4, 3)[2])
-    #     # print(tr.difference(digit_canceling_fractions_4_3()[2]))
-    # #     print((3964, 6937, 4, 7) in digit_canceling_fractions_4_3()[2])
-    # #     print((4132, 7231, 4, 7) in digit_canceling_fractions_4_3()[2])
-    # #     print(digit_canceling_fractions_4_3())
-    #     print(*digit_canceling_fractions_4_3()[:2])
-
-    def test_digit_canceling_fractions_4_1(self):
-        # start = time.time()
-        # print(digit_canceling_fractions_4_1())
-        # end = time.time()
-        # elapsed = end - start
-        # print('time elapsed: ', elapsed)
-        self.assertEqual(digit_canceling_fractions_4_1()[:2], (12999936, 28131911))
-
-    def test_digit_canceling_fractions_4_2(self):
-        # start = time.time()
-        # print(digit_canceling_fractions_4_2())
-        # end = time.time()
-        # elapsed = end - start
-        # print('time elapsed: ', elapsed)
-        self.assertEqual(digit_canceling_fractions_4_2()[:2], (3571225, 7153900))
+    def test_digit_canceling_fractions(self):
+        self.assertEqual(digit_canceling_fractions(4, 2), (3571225, 7153900))
+        self.assertEqual(digit_canceling_fractions(4, 1), (12999936, 28131911))
+        self.assertEqual(digit_canceling_fractions(4, 3), (255983, 467405))
+        self.assertEqual(digit_canceling_fractions(3, 1), (77262, 163829))
+        self.assertEqual(digit_canceling_fractions(2, 1), (110, 322))
 
 
 if __name__ == '__main__':
