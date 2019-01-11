c404407a44fc3840d8920464bf10569f6f1a6ee3
Michael Angel <mike006322@gmail.com>
2019-01-11 14:57:11 -0800

    PE044_pentagon_numbers

--- Solutions/PE044_pentagon_numbers/pentagon_numbers.py
+++ Solutions/PE044_pentagon_numbers/pentagon_numbers.py
@@ -9,25 +9,55 @@
 # P_n - P_n-K = (3n^2 - n)/2 - (3n^2 - 6nK - n + 3K^2 + K)/2
 # = (3n^2 - n - 3n^2 + 6nK + n - 3K^2 - K)/2
 # = (6nK - 3K^2 - K)/2
-# Question: for constant K, does (3n^2 - n)/2 = (6nK - 3K^2 - K)/2 have integer solution?
-# i.e. 3n^2 + (-6K-1)n + 3K^2 + K = 0 have integer solution?
-# 3n^2 + (-6K-1)n + 3K^2 + K = 0 solve for n: n = K or n = K + 1/3
-# quadratic formula K-1 +- (((6K-1)**2 - 12(3K**2 + K))**.5)/6
+# P_n + P_n-K = (3n^2 - n)/2 + (3n^2 - 6nK - n + 3K^2 + K)/2
+# = (3n^2 - n + 3n^2 - 6nK - n + 3K^2 + K)/2
+# = (6n^2 - 2n(1 - 3K) + 3K^2 + K)/2
+# Question: for constant K, which n's grant that (3m^2 - m)/2 = (6nK - 3K^2 - K)/2 has integer solution?
+# 3m^2 - m = 6nK - 3K^2 - K
+# 0 = - 3m^2 + m + 6nK - 3K^2 - K
+# solving for m:
+# -1 +- (1 + 12(6nK - 3K^2 - K))**.5 all over -6
+# 1 -+ (1 + 12(6nK - 3K^2 - K))**.5 all over 6
+# happens when 1 -+ (1 + 12(6nK - 3K^2 - K))**.5 = 6r for some r
+# (1 + 12(6nK - 3K^2 - K))**.5 = 6r +- 1
+# 1 + 12(6nK - 3K^2 - K) = (6r +- 1)^2
+
 
 
 def pentagonal_number(n):
     """
     returns P_n = n(3n-1)/2 = (3n^2 - n)/2
     """
-    return (3*n**2 - n)/2
+    return (3*n**2 - n)//2
+
+
+def is_pentagonal(n):
+    """
+    returns boolean whether n is a solution to (3*m**2 - m)
+    """
+    # n = 3*m**2 - m ? for some m
+    # 0 = 3*m**2 - m - n
+    # (1 +- (1 + 12*n)**.5)/6
+    if 1 + 12*n > 1:
+        if (1 + (1 + 24*n)**.5)/6 == (1 + (1 + 24*n)**.5)//6:
+            return True
+    return False
 
 
 def pentagonal_numbers(N, K):
     """
     returns pentagonal_number(n) for all n < N such that P_n - P_n-K is pentagonal
     """
-    pass
+    for n in range(K+1, N):
+        # test whether (3n^2 - n)/2 - (3n^2 - 6nK - n + 3K^2 + K)/2 is pentagonal
+        P_n_minus_P_n_K = (6*n*K - 3*K**2 - K)//2
+        if is_pentagonal(P_n_minus_P_n_K):
+            print(pentagonal_number(n))
+        P_n_plus_P_n_K = pentagonal_number(n) + pentagonal_number(n-K)
+        if is_pentagonal(P_n_plus_P_n_K):
+            print(pentagonal_number(n))
 
 
 if __name__ == '__main__':
-    pass
+    # pentagonal_numbers(50, 8)
+    pentagonal_numbers(*map(int, input().split()))
--- Solutions/PE044_pentagon_numbers/test_pentagon_numbers.py
+++ Solutions/PE044_pentagon_numbers/test_pentagon_numbers.py
@@ -9,6 +9,16 @@ class Test(unittest.TestCase):
         self.assertEqual(pentagonal_number(2), 5)
         self.assertEqual(pentagonal_number(3), 12)
 
+    def test_is_pentagonal(self):
+        p = {1, 5, 12, 22, 35, 51, 70, 92}
+        for i in range(1, 100):
+            if i in p:
+                self.assertTrue(is_pentagonal(i))
+            else:
+                if is_pentagonal(i):
+                    print(i)
+                self.assertFalse(is_pentagonal(i))
+
 
 if __name__ == '__main__':
     unittest.main()
