eee700c2eb551434b64f9613a441ef4b09bd1a9f
Michael Angel <mike006322@gmail.com>
2019-01-17 18:21:31 -0800

    C++ version

---
+++ Solutions/PE001_multiples_of_3_and_5/cpp/MultiplesOf3And5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+long int SumOneTo(long int n){
+    return n*(n+1)/2;
+}
+
+long int MultiplesOf3And5(long int n){
+    long int NumberOfMultiplesOf3, NumberOfMultiplesOf5, NumberOfMultiplesOf15;
+    long int SumOfMultiplesOf3, SumOfMultiplesOf5, SumOfMultiplesOf15;
+    NumberOfMultiplesOf3 = (n-1)/3;
+    NumberOfMultiplesOf5 = (n-1)/5;
+    NumberOfMultiplesOf15 = (n-1)/15;
+    SumOfMultiplesOf3 = 3*SumOneTo(NumberOfMultiplesOf3);
+    SumOfMultiplesOf5 = 5*SumOneTo(NumberOfMultiplesOf5);
+    SumOfMultiplesOf15 = 15*SumOneTo(NumberOfMultiplesOf15);
+    return SumOfMultiplesOf3 + SumOfMultiplesOf5 - SumOfMultiplesOf15;
+}
+
+int main()
+{
+    int t;
+
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        cout << MultiplesOf3And5(n) << endl;
+    }
+    return 0;
+}
--- Solutions/PE001_multiples_of_3_and_5/python/test_multiples_of_3_and_5.py
+++ Solutions/PE001_multiples_of_3_and_5/python/test_multiples_of_3_and_5.py
@@ -1,10 +1,12 @@
 import unittest
-from multiples_of_3_and_5 import *
+from Solutions.PE001_multiples_of_3_and_5.python.multiples_of_3_and_5 import *
+
 
 class Test(unittest.TestCase):
 
     def test_multiples_of_3_and_5(self):
         self.assertEqual(multiples_of_3_and_5(15), 45)
+
 
 if __name__ == '__main__':
     unittest.main()
