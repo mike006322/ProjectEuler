276948fa7d3b7d0b93418f455fbc1a0ed8e13f85
Michael Angel <mike006322@gmail.com>
2019-01-28 20:16:28 -0800

    added javascript implementation

---
+++ Algorithms/javascript/sieveOfEratosthenes.js
@@ -0,0 +1,12 @@
+function sieveOfEratosthenes(c) {
+    let a=[], i, j, b=[];
+    for (i = 2; i <= c; ++i) {
+        if (!a[i]) {
+            b.push(i);
+            for (j = i << 1; j <= c; j += i) {
+                a[j] = true;
+            }
+        }
+    }
+    return b;
+}
