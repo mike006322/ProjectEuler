4dfd33e902ba1fb78e93e085e9778a64103c39c7
Michael Angel <mike006322@gmail.com>
2019-01-28 20:29:31 -0800

    changed variable names, added comment

--- Algorithms/javascript/sieveOfEratosthenes.js
+++ Algorithms/javascript/sieveOfEratosthenes.js
@@ -1,14 +1,15 @@
-function sieveOfEratosthenes(c) {
-    let a=[], i, j, b=[];
-    for (i = 2; i <= c; ++i) {
-        if (!a[i]) {
-            b.push(i);
-            for (j = i << 1; j <= c; j += i) {
-                a[j] = true;
+function sieveOfEratosthenes(n) {
+    //returns array of primes up to n
+    let sieve=[], i, j, res=[];
+    for (i = 2; i <= n; ++i) {
+        if (!sieve[i]) {
+            res.push(i);
+            for (j = i << 1; j <= n; j += i) {
+                sieve[j] = true;
             }
         }
     }
-    return b;
+    return res;
 }
 
 module.exports.sieveOfEratosthenes = sieveOfEratosthenes;
