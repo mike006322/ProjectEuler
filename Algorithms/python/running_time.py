63ae18e0e415d783ffc120e627245667cb454aae
Michael Angel <mike006322@gmail.com>
2019-03-15 14:39:38 -0700

    made 'running_time' function that adds 'running_time' string to function

--- Algorithms/python/running_time.py
+++ Algorithms/python/running_time.py
@@ -1,16 +1,16 @@
 from time import time
 
 
-def time_function(f, i):
-    """
-    returns the time it takes to run function f with input i
-    """
-    start = time()
-    f(*i)
-    end = time()
-    elapsed = end - start
-    print(elapsed, ' seconds have elapsed for ', f,)  # ' with input ', i)
-    return elapsed
+def running_time(f):
+    # decorator adds running time as a function variable
+    def new_f(*args, **kwargs):
+        start = time()
+        res = f(*args, **kwargs)
+        end = time()
+        elapsed = end - start
+        new_f.running_time = str(elapsed) + ' seconds have elapsed for ' + str(f.__name__) + '(' + str(*args) + str(**kwargs) + ')'
+        return res
+    return new_f
 
 
 def get_div_sum(n):
@@ -68,13 +68,13 @@ def fast_sum_of_proper_divisors(n, primes, primesset):
 if __name__ == '__main__':
     from Algorithms.python.Sieve_of_Eratosthenes import Sieve_of_Eratosthenes_numpy
 
-    # time_function(Sieve_of_Eratosthenes, 1000000)
-    # time_function(fast_Sieve_of_Eratosthenes, 1000000)
-    # time_function(Sieve_of_Eratosthenes_numpy, 1000000)
+    Sieve_of_Eratosthenes_numpy = running_time(Sieve_of_Eratosthenes_numpy)
+    Sieve_of_Eratosthenes_numpy(6000000)
+    print(Sieve_of_Eratosthenes_numpy.running_time)
 
-    primes = Sieve_of_Eratosthenes_numpy(1000000000)
-    primesset = set(primes)
-    print(primes[-1])
-    time_function(get_div_sum, [1000000000])
-    time_function(fast_sum_of_proper_divisors, (999999937, primes, primesset))
+    # primes = Sieve_of_Eratosthenes_numpy(1000000000)
+    # primesset = set(primes)
+    # print(primes[-1])
+    # time_function(get_div_sum, [1000000000])
+    # time_function(fast_sum_of_proper_divisors, (999999937, primes, primesset))
 
--- Solutions/PE048_self_powers/test_self_powers.py
+++ Solutions/PE048_self_powers/test_self_powers.py
@@ -1,13 +1,18 @@
 import unittest
+from Algorithms.python.running_time import *
 from Solutions.PE048_self_powers.self_powers import *
+
+self_powers = running_time(self_powers)
 
 
 class Test(unittest.TestCase):
 
     def test_self_powers(self):
+
+        print(self_powers(10))
         self.assertEqual(self_powers(10), 405071317)
-        # print(time_function(self_powers, [10**5+100]))
-        # print(self_powers(1000))
+        self_powers(10**5+100)
+        print(self_powers.running_time)
 
 
 if __name__ == '__main__':
