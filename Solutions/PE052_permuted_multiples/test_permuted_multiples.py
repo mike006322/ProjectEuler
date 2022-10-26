51382187c551f648cf77c090ace26c271b6a7b9f
MikeAngel <mike00632@gmail.com>
2022-10-25 21:32:29 -0700

    adds commented lines that help find answer for official PE site

--- Solutions/PE052_permuted_multiples/test_permuted_multiples.py
+++ Solutions/PE052_permuted_multiples/test_permuted_multiples.py
@@ -7,6 +7,8 @@ class Test(unittest.TestCase):
     def test_permuted_multiples(self):
         res = "125874 251748 "
         self.assertEqual('\n'.join(permuted_multiples(125875, 2)), res)
+        # for item in permuted_multiples(1258750, 6):
+        #     print(item)
 
 
 if __name__ == '__main__':
